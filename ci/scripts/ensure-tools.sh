d684e9323d3fdc66d82e7829e21862be7acf7bdf
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-02-26 10:04:18 -0500

    Update ensure-tools.sh

--- ci/scripts/ensure-tools.sh
+++ ci/scripts/ensure-tools.sh
@@ -30,6 +30,7 @@ wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sh
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install vault
 
+echo $(ls -la /bin/bosh)
 
 echo "installed:"
 echo "bosh: $(bosh --version)"
