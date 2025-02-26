a9a005f9f81ed87e913cffd1880fe54ae02a5a0a
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-02-26 09:54:27 -0500

    Update ensure-tools.sh

--- ci/scripts/ensure-tools.sh
+++ ci/scripts/ensure-tools.sh
@@ -29,3 +29,12 @@ chmod u+x /bin/genesis
 wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install vault
+
+
+echo "installed:"
+echo "bosh: $(bosh --version)"
+echo "credhub: $(credhub --version)"
+echo "safe: $(safe --version)"
+echo "spruce: $(spruce --version)"
+echo "genesis: $(genesis --version)"
+echo "vault: $(vault --version)"
